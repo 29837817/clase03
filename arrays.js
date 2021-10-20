b86fe9a71354a9ad426a4055cb4629b581e404ad
Lorena <cejaaimogasta@gmail.com>
2021-10-20 06:56:18 +0200

    clase

---
+++ arrays.js
@@ -0,0 +1,24 @@
+//creacion de array
+
+let listaDeFrutas = ['banana', 'manzana', 'frutilla', 'pera']
+//console.log (listaDeFrutas)
+
+let notasMateria = [8, 10, 4, 7]
+//console.log (notasMateria)
+
+//modificar array
+
+listaDeFrutas [0] = 'Mango';
+console.log (listaDeFrutas)
+
+console.log ()
+
+notasMateria [3] = 2
+console.log (notasMateria)
+
+//Posiciones
+
+console.log (listaDeFrutas [2])
+console.log (notasMateria [3])
+
+
---
+++ objeto.js
@@ -0,0 +1,16 @@
+//creación
+let auto= {
+    modelo: 'Ford',
+    año: 2020,
+    motor: 1.6,
+    servis: [2019, 2021, 2020]
+}
+
+
+console.log (auto.servis[0]) 
+
+
+
+//modificar atributo
+auto.modelo = 'Chevrolet'
+console.log(auto)
